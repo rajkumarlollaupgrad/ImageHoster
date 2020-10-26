eeb5bf2d18dc89ba959ccbdd6247655744ef29b4
Rajkumar Lolla <rajkumarlollaupgrad@github.com>
2020-10-26 19:02:29 +0530

    Added new classes (CommentController, CommentService,CommentRepository and Comment Model) to handle the comments given by users
    Added new Controller "CommentController" to handle the Comments given by users.

---
+++ ImageHoster/src/main/java/ImageHoster/service/CommentService.java
@@ -0,0 +1,23 @@
+package ImageHoster.service;
+
+import ImageHoster.model.Comments;
+import ImageHoster.model.Image;
+import ImageHoster.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Qualifier("commentService")
+public class CommentService {
+
+    @Autowired
+    CommentRepository commentRepository;
+
+    public void addComments(Comments comments){ commentRepository.addComments(comments);}
+
+    public List<Comments> retrieveComments(Image image){ return commentRepository.retrieveComments(image);}
+}
+
